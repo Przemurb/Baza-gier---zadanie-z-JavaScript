31fe527ba4f7f95a71980c85552fde695acd1c3f
Przemurb <przemurb@hotmail.it>
2022-11-21 16:14:19 +0100

    - dodano dane startwe - przebudowano funkcję dodawania wiersza

--- script.js
+++ script.js
@@ -8,19 +8,27 @@ class Game {
 
 let games = [];
 
+// DANE TESTOWE
+addExample();
+games.forEach((e) => {
+    createNewRow(e.title, e.vote);
+});
+
 
 function registerAddButton() {
     let addButton = document.getElementById("submit-button");
     addButton.addEventListener("click", addToList)
 }
 
+registerAddButton();
+
 function addToList() {
     let title = document.getElementById("title").value;
     let vote = document.getElementById("vote").value;
     let description = document.getElementById("description").value;
     clearDescription();
-    if (title != "" & vote != "" & description != "") {
-        createNewRow();
+    if (title !== "" & vote !== "" && description !== "") {
+        createNewRow(title, vote);
         saveGame();
         clearForm();
     } else {
@@ -33,29 +41,30 @@ function addToList() {
         document.getElementById("description").classList.add("input-error");
     }
 
-    function createNewRow() {
-        let table = document.getElementById("table");
-        let row = table.insertRow();
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        let cell3 = row.insertCell(2);
-        let cell4 = row.insertCell(3);
-
-        cell1.innerHTML = title;
-        cell2.innerHTML = vote;
-        cell3.innerHTML = "<button id='details-button' onclick='showDescription(this)'>Pokaż szczegóły</button>";
-        cell4.innerHTML = "<button id='remove-button' onclick='deleteFromList(this)'>Usuń</button>";
-    }
 
     function saveGame() {
         games.push(new Game(title, vote, description));
     }
 }
 
-registerAddButton();
+function createNewRow(title, vote) {
+    let table = document.getElementById("table");
+    let row = table.insertRow();
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+
+    cell1.innerHTML = title;
+    cell2.innerHTML = vote;
+    cell3.innerHTML = "<button id='details-button' onclick='showDescription(this)'>Pokaż szczegóły</button>";
+    cell4.innerHTML = "<button id='remove-button' onclick='deleteFromList(this)'>Usuń</button>";
+}
+
 
 function deleteFromList(r) {
     clearForm();
+    clearDescription();
     let table = document.getElementById("table");
     let i = r.parentNode.parentNode.rowIndex;
     games.splice(i - 1, 1);
@@ -78,20 +87,14 @@ function clearForm() {
     document.getElementById("title").value = null;
     document.getElementById("vote").value = null;
     document.getElementById("description").value = null;
-    clearError();
-
-    function clearError() {
-        document.getElementById("title").classList.remove("input-error");
-        document.getElementById("vote").classList.remove("input-error");
-        document.getElementById("description").classList.remove("input-error");
-    }
-
+    document.getElementById("title").classList.remove("input-error");
+    document.getElementById("vote").classList.remove("input-error");
+    document.getElementById("description").classList.remove("input-error");
 }
 
 
-
-// function addExample() {
-//     games.push(new Game("Wiedźmin 3", 10, "Fantastyczna gra o walce z potworami i jeżdzeniu na koniu."));
-//     games.push(new Game("Starcraft", 7, "Walki kosmitów i różnych ras."));
-//     games.push(new Game("The Sims", 8, "Buduj domy, rozwijaj relacje. Prawdziwe życie w realnym świecie."));
-// }
+function addExample() {
+    games.push(new Game("Wiedźmin 3", 10, "Fantastyczna gra o walce z potworami i jeżdzeniu na koniu."));
+    games.push(new Game("Starcraft", 7, "Walki kosmitów i różnych ras."));
+    games.push(new Game("The Sims", 8, "Buduj domy, rozwijaj relacje. Prawdziwe życie w realnym świecie."));
+}
